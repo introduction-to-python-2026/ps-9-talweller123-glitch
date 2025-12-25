d6f2ba748728ac1eb897a86cbfcca69b92e9aac4
talweller123-glitch <talweller123@gmail.com>
2025-12-25 16:39:24 +0200

    Update main.py 1

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
+!wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
+import lab_setup_do_not_edit
+
+import pandas as pd
+
+df = pd.read_csv('parkinsons.csv')
+df = df.dropna()
+
+df.columns 
+
+independents = ['MDVP:Shimmer(dB)', 'PPE']
+dependent = 'status'
+x = df[independents]
+y = df[dependent]
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+x_scalared = scaler.fit_transform(x)
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x_scalared, y, test_size=0.2, random_state=42)
+
+from sklearn.tree import DecisionTreeClassifier
+model = DecisionTreeClassifier(max_depth = 3, random_state = 42)
+model.fit(x_train, y_train)
+
+from sklearn.metrics import accuracy_score
+
+y_pred = model.predict(x_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy}")
 
